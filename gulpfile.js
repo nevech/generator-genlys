d28ae8349a7f9e9024a770501030bf3e43306564
Ruslan Nevecherya <r.nevech@gmail.com>
2015-05-03 17:25:17 +0300

    Add task compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
+var browserSync = require('browser-sync');
+var wiredep = require('wiredep').stream;
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var del = require('del');
 
-var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 var env = process.env.NODE_ENV || 'development';
 var isProduction = false;
+var destDir = '.tmp';
 var appName = 'myApp';
 
 var $ = require('gulp-load-plugins')({
@@ -14,13 +16,21 @@ var $ = require('gulp-load-plugins')({
   scope: ['dependencies', 'devDependencies']
 });
 
+function setENV () {
+  // Set env from args
+  if (args.production) {
+    isProduction = true;
+    env = 'production';
+  }
+}
+
 gulp.task('clean', function (cb) {
-  del('dist', cb);
+  del(['dist', '.tmp'], cb);
 });
 
-gulp.task('assets', ['extras'], function () {
+gulp.task('assets', function () {
   return gulp.src('app/assets/**/*')
-    .pipe(gulp.dest('./dist/assets'));
+    .pipe(gulp.dest(destDir + '/assets'));
 });
 
 gulp.task('extras', function () {
@@ -30,25 +40,39 @@ gulp.task('extras', function () {
     '!app/config.json',
   ], {
     dot: true
-  }).pipe(gulp.dest('./dist'));
+  }).pipe(gulp.dest(destDir));
 });
 
-gulp.task('scripts', function () {
-  return gulp.src('app/scripts/**/*.coffee')
-    .pipe($.coffee({
-      bare: true
-    }))
-    .pipe($.ngAnnotate())
-    .pipe($.if(isProduction, $.uglify().on('error', $.util.log)))
-    .pipe(gulp.dest('dist/scripts'));
+gulp.task('fonts', function () {
+  return gulp.src(require('main-bower-files')({
+    filter: '**/*.{eot,svg,ttf,woff,woff2}'
+  }).concat('app/assets/fonts//**/*'))
+    .pipe(gulp.dest(destDir + '/assets/fonts'));
 });
 
-gulp.task('config', ['scripts'], function () {
+gulp.task('config', function () {
   return gulp.src('app/config.json')
     .pipe($.ngConfig(appName + '.config', {
       environment: env
     }))
-    .pipe(gulp.dest('dist/scripts/configs'))
+    .pipe(gulp.dest('./.tmp/scripts/configs'))
+});
+
+gulp.task('scripts', ['config'], function () {
+  var filterCoffee = $.filter('**/*.coffee');
+
+  return gulp.src([
+      'app/scripts/**/*.coffee',
+      'app/scripts/**/*.js'
+    ])
+    .pipe(filterCoffee)
+    .pipe($.coffee({
+      bare: true
+    }))
+    .pipe(filterCoffee.restore())
+    .pipe($.ngAnnotate())
+    .pipe($.if(isProduction, $.uglify().on('error', $.util.log)))
+    .pipe(gulp.dest('./.tmp/scripts'));
 });
 
 gulp.task('styles', function () {
@@ -64,19 +88,84 @@ gulp.task('styles', function () {
     .pipe($.autoprefixer({
       browsers: ['> 0.5%', 'ie 8', 'Opera 11.5']
     }))
-    .pipe(gulp.dest('./dist/styles'));
+    .pipe(gulp.dest('./.tmp/styles'));
+});
+
+gulp.task('wiredep', function () {
+  return gulp.src('app/index.jade')
+    .pipe(wiredep({
+      directory: './bower_components',
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('jade', ['wiredep'], function () {
+  return gulp.src('app/**/*.jade')
+    .pipe($.jade({
+      pretty: true
+    }))
+    .on('error', $.util.log)
+    .pipe(gulp.dest('./.tmp'));
+});
+
+gulp.task('compile', ['jade', 'scripts', 'styles'], function () {
+  var assets = $.useref.assets({searchPath: ['.', '.tmp']});
+
+  return gulp.src('.tmp/**/*.html')
+    .pipe(assets)
+    .pipe($.if('*.js', $.uglify(), $.rev() ))
+    .pipe($.if('*.css', $.minifyCss(), $.rev()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe($.minifyHtml({
+      empty: true,
+      spare: true
+    }))
+    .pipe($.revReplace())
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('build', ['clean'], function () {
-  // Set env from args
-  if (args.production) {
-    isProduction = true;
-    env = 'production';
-  }
+  setENV();
+  destDir = 'dist';
 
   gulp.start([
     'assets',
-    'config',
-    'styles',
-  ]);
+    'extras',
+    'compile'
+  ], function () {
+    del('.tmp');
+  });
+
 });
+
+gulp.task('serve:dist', function () {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['dist'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+});
+
+gulp.task('serve', ['clean'], function () {
+  setENV();
+
+  gulp.start([
+    'assets',
+    'extras',
+    'jade',
+    'scripts',
+    'styles',
+  ], function () {
+
+  });
+
+});
+
+gulp.task('default', ['build']);
