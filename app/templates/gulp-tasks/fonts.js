887cecd1e311829c7b67353a7cff1f46ccb23ef4
nevech <r.nevech@gmail.com>
2015-07-09 11:17:42 +0300

    add templates gulp-tasks

---
+++ app/templates/gulp-tasks/fonts.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+var config = require('../configs/');
+var mainBowerFiles = require('main-bower-files');
+var reload = require('browser-sync').reload;
+
+function getGlobFonts () {
+  var glob = mainBowerFiles({
+    filter: '**/*.{eot,svg,ttf,woff,woff2}'
+  });
+
+  glob.concat('app/public/fonts/**/*');
+
+  return glob;
+}
+
+gulp.task('fonts', function () {
+  var glob = getGlobFonts();
+
+  return gulp.src(glob)
+    .pipe(gulp.dest(config.destDir + '/fonts'));
+});
+
+gulp.task('fonts:watch', function () {
+  var glob = getGlobFonts();
+
+  return gulp.src(glob)
+    .pipe(watch(glob, {verbose: true}))
+    .pipe(gulp.dest(config.buildDir + '/fonts'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:dist', function () {
+  var glob = getGlobFonts();
+
+  return gulp.src(glob)
+    .pipe(gulp.dest(config.buildDir + '/fonts'));
+});
---
+++ app/templates/gulp-tasks/styles.js
@@ -0,0 +1,43 @@
+var config = require('../configs/');
+var lazypipe = require('lazypipe');
+var reload = require('browser-sync').reload;
+
+var gulp = require('gulp'),
+    gulpif = require('gulp-if'),
+    watch = require('gulp-watch'),
+    filter = require('gulp-filter'),
+    stylus = require('gulp-stylus'),
+    autoprefixer = require('gulp-autoprefixer');
+
+var stylesTasks = lazypipe()
+  .pipe(function autoPrefixer() {
+    return autoprefixer(config.autoprefixer);
+  })
+  .pipe(function () {
+    return gulp.dest(config.destDir + '/styles');
+  })
+  .pipe(function () {
+    return reload({stream: true});
+  });
+
+gulp.task('styles', function () {
+  return gulp.src(config.paths.styles)
+    .pipe(gulpif('*.styl', stylus()))
+    .pipe(autoprefixer(config.autoprefixer))
+    .pipe(gulp.dest(config.destDir + '/styles'));
+});
+
+gulp.task('css:watch', function () {
+  return gulp.src(config.paths.css)
+    .pipe(watch(config.paths.css, {verbose: true}))
+    .pipe(stylesTasks());
+});
+
+gulp.task('stylus:watch', function () {
+  return gulp.src(config.paths.stylus)
+    .pipe(watch(config.paths.stylus, {verbose: true}))
+    .pipe(stylus())
+    .pipe(stylesTasks());
+});
+
+gulp.task('styles:watch', ['css:watch', 'stylus:watch']);
