3dfa686dc035b1c0f8bf823c5cd545a3fbe71c0b
nevech <r.nevech@gmail.com>
2015-07-23 18:04:34 +0300

    move task serve to gulp-tasks/serve.js from gulpfile

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,13 +1,7 @@
-var browserSync = require('./.genlys/browser-sync');
 var gulp = require('gulp');
-var del = require('del');
-var gulpsync = require('gulp-sync')(gulp);
-var modRewrite  = require('connect-modrewrite');
-
+var plumber = require('gulp-plumber');
+var prompt = require('gulp-prompt');
 var config = require('./.genlys/config');
-
-// load gulp plugins
-var $ = require('gulp-load-plugins')(config.optionLoadPlugins);
 
 // require all task from gulp-tasks dir
 require('require-dir')('./.genlys/gulp-tasks');
@@ -16,66 +10,12 @@ require('require-dir')('./.genlys/gulp-tasks');
 var _gulpsrc = gulp.src;
 gulp.src = function() {
   return _gulpsrc.apply(gulp, arguments)
-    .pipe($.plumber());
+    .pipe(plumber());
 };
-
-gulp.task('clean:serve', function (cb) {
-  del(config.destDir, cb);
-});
-
-var serveTasks = [
-  'assets',
-  'fonts',
-  'images',
-  'templates',
-  'styles',
-  'scripts',
-  'ngConfig'
-];
-
-gulp.task('serve', gulpsync.sync(['clean:serve', serveTasks]), function () {
-  browserSync.init({
-    notify: false,
-    port: config.port,
-    server: {
-      baseDir: ['.tmp'],
-      routes: {
-        '/bower_components': 'bower_components'
-      },
-      middleware: [
-        modRewrite([
-          '!\\.\\w+\?.*$ /index.html [L]'
-        ])
-      ]
-    }
-  });
-
-  gulp.start([
-    'styles:watch',
-    'templates:watch',
-    'scripts:watch',
-    'images:watch',
-    'fonts:watch',
-    'assets:watch'
-  ]);
-
-  gulp.watch('bower.json', ['wiredep', 'fonts', browserSync.reload]);
-});
-
-
-gulp.task('serve:dist', function () {
-  browserSync.init({
-    notify: false,
-    port: config.port,
-    server: {
-      baseDir: ['dist'],
-    }
-  });
-});
 
 gulp.task('default', function () {
   gulp.src('package.json')
-    .pipe($.prompt.prompt({
+    .pipe(prompt.prompt({
       type: 'list',
       name: 'task',
       message: 'What do you want to do?',
