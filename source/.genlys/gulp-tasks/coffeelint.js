03c0136a86ee263b43961fef083cd804d5212303
nevech <r.nevech@gmail.com>
2015-11-02 12:44:24 +0200

    remove coffeelint-stylish

--- source/.genlys/gulp-tasks/coffeelint.js
+++ source/.genlys/gulp-tasks/coffeelint.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var guppy = require('git-guppy')(gulp);
 var coffeelint = require('gulp-coffeelint');
-var stylish = require('coffeelint-stylish');
 var config = require('../config');
 var path = require('path');
 
@@ -10,7 +9,6 @@ var pathToLintConfig = path.resolve(__dirname, '../../', 'configs/coffeelint.jso
 gulp.task('lint', function () {
   gulp.src(config.paths.coffee)
     .pipe(coffeelint(pathToLintConfig, null, true))
-    .pipe(coffeelint.reporter(stylish))
     .pipe(coffeelint.reporter('fail'));
 });
 
